9463623ff1566772d80eaebc3692d0d100fd5634
kk-spartans <kcube.jan+github@gmail.com>
2025-10-17 23:42:44 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'em-internals';
+
 const nextConfig: NextConfig = {
-    output: 'export',
-    images: { unoptimized: true }
+  output: 'export', 
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? './' : '',
 };
 
 export default nextConfig;
